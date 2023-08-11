d1e4c994ff19b35daccd785daadd98927dd564ee
Anton <avodichenkov@gmail.com>
2023-08-12 00:32:19 +0300

    Test

---
+++ JvStartPoint/src/main/java/org/jvchat/App.java
@@ -0,0 +1,14 @@
+package org.jvchat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Arr1 a = new Arr1();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ untitled/src/main/java/org/jvchat/Arr1.java
@@ -0,0 +1,17 @@
+package org.jvchat;
+
+/**
+ * Hello world!
+ *
+ */
+public class Arr1
+{
+    Arr1() {
+        System.out.println( "Hello World! lox" );
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! lox" );
+    }
+
+}
