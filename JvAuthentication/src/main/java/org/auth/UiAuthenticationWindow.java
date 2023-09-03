51ef836bf8fff59fdb440dd8fc4e20c743c31914
Anton <avodichenkov@gmail.com>
2023-09-03 21:17:46 +0300

    Центрирование

--- JvAuthentication/src/main/java/org/auth/UiAuthenticationWindow.java
+++ JvAuthentication/src/main/java/org/auth/UiAuthenticationWindow.java
@@ -9,17 +9,20 @@ public class UiAuthenticationWindow extends JFrame {
     public UiAuthenticationWindow () {
         setTitle("Вход");
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.setLayout( new BoxLayout(panel, BoxLayout.PAGE_AXIS ) );
 
-        JButton button1 = new JButton("Top");
-        panel.add(button1);
+        JTextField button1 = new JTextField( "Top" );
+        button1.setAlignmentX( Component.CENTER_ALIGNMENT );
+        panel.add( button1 );
 
-        JButton button2 = new JButton("Center");
-        panel.add(button2);
+        JTextField button2 = new JTextField( "Center" );
+        button2.setAlignmentX( Component.CENTER_ALIGNMENT );
+        panel.add( button2 );
 
-        JButton button3 = new JButton("Bottom");
-        panel.add(button3);
-        getContentPane().add(panel);
+        JButton bEnter = new JButton( "Войти" );
+        bEnter.setAlignmentX( Component.CENTER_ALIGNMENT );
+        panel.add( bEnter );
+        getContentPane().add( panel );
 
 
 //        getContentPane().setLayout( new BoxLayout( getContentPane(), BoxLayout.PAGE_AXIS ) );
@@ -27,12 +30,12 @@ public class UiAuthenticationWindow extends JFrame {
 //        bEnter.setSize( ( int ) Math.round( 0.25 * getContentPane().getWidth() ),
 //                ( int ) Math.round( 0.25 * getContentPane().getHeight() ) );
 //        getContentPane().add(bEnter);
-        button3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        bEnter.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
 
-                System.out.println("kokokokokok");
+                System.out.println( "kokokokokok" );
             }
-        });
+        } );
 
     }
 
