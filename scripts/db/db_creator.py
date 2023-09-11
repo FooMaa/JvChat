5f886c967f819648ba177a83af3fa912bde37afe
unknown <avodichenkov@gmail.com>
2023-09-11 13:46:56 +0300

    start db

---
+++ scripts/db/db_creator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import base64
+import psycopg2
+
+
+ADMIN_USER = 'postgres'
+ADMIN_DB = 'postgres'
+ADMIN_PWD = base64.b64decode('1234567890').decode('utf-8')
+DEFAULT_DB_USER = 'jvchat'
+DB_USER_PWD = base64.b64decode('0987654321').decode('utf-8')
+DEFAULT_DB_IP = '127.0.0.1'
+DEFAULT_DB_NAME = 'chat'
+DEFAULT_DB_TEMPLATE = 'chat_template'
+DEFAULT_BACKUP_FOLDER = '/tmp/'
+DEFAULT_BACKUP = "chat_dump.bak"
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+END = '\033[0m'
+PENDING = '[...] '
+SUCCESS = "\r[ " + GREEN + "OK" + END + " ] "
+FAIL = "\r[ " + RED + "FAIL" + END + " ] "
+FNULL = open(os.devnull, 'w')
