64eb05aceae955409064cf0918cf3e4c2bb62013
anton <avodichenkov@gmail.com>
2023-09-12 18:07:51 +0300

    t

--- scripts/db/pre_inst_db.sh
+++ scripts/db/pre_inst_db.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 DIR=$(realpath $0 | sed -e "s/pre_inst_db.*//g")
+POST_PWD='9999'
 echo $DIR
 
+
+apt-get update
+apt-get install -y $(cat requirements)
+
+echo -e "$POST_PWD\n$POST_PWD" | passwd postgres
+# copy
+service postgresql restart
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD '$POST_PWD';"
