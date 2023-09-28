742980ccf80fff5f87631d7d85f311bdb68e32c8
Anton <avodichenkov@gmail.com>
2023-09-28 13:31:45 +0300

    Начал более грамотно писать db_creator

--- scripts/db/1_create_users.sql
+++ scripts/db/1_create_users.sql
@@ -3,14 +3,12 @@ SET CLIENT_ENCODING TO 'UTF-8';
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-    user_id     bigint,
---    parent_id   bigint,
-    name        character varying(32),
-    PRIMARY KEY ( id )
---    FOREIGN KEY (parent_id) REFERENCES parent (id) MATCH SIMPLE
---        ON UPDATE CASCADE ON DELETE CASCADE
-)
+    user_id     bigint, --    parent_id   bigint,
+    name        character varying(32), --    FOREIGN KEY (parent_id) REFERENCES parent (id) MATCH SIMPLE
+    PRIMARY KEY ( user_id ) --        ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-COMMENT ON TABLE users.user_id IS "INFO";
+COMMENT ON TABLE users IS 'Логин и пароль пользователя';
+COMMENT ON COLUMN users.user_id IS 'ID пользователя';
 
 INSERT INTO users (user_id, name) VALUES (0, 'Anton');
--- scripts/db/db_creator.py
+++ scripts/db/db_creator.py
@@ -2,14 +2,15 @@
 # coding=utf-8
 
 import base64
-import psycopg2
+import sys
 
+import psycopg2
 
 ADMIN_USER = 'postgres'
 ADMIN_DB = 'postgres'
-#ADMIN_PWD = base64.b64decode('1234567890').decode('utf-8')
+ADMIN_PWD = '9999'  # base64.b64decode('1234567890').decode('utf-8')
 DEFAULT_DB_USER = 'jvchat'
-#DB_USER_PWD = base64.b64decode('0987654321').decode('utf-8')
+DB_USER_PWD = '1111'
 DEFAULT_DB_IP = '127.0.0.1'
 DEFAULT_DB_NAME = 'chat'
 DEFAULT_DB_TEMPLATE = 'chat_template'
@@ -23,9 +24,24 @@ PENDING = '[...] '
 SUCCESS = "\r[ " + GREEN + "OK" + END + " ] "
 FAIL = "\r[ " + RED + "FAIL" + END + " ] "
 
-conn = psycopg2.connect (user = ADMIN_USER, password = '9999', host = "127.0.0.1")
-curs = conn.cursor()
-conn.autocommit = True
-s = "CREATE DATABASE test;"
-curs.execute(s)
 
+class DataBase:
+    def __init__(self):
+        sys.stdout.write(PENDING + "create connection")
+        self.connection = psycopg2.connect(user=ADMIN_USER, password=ADMIN_PWD, host="127.0.0.1")
+        self.cursor = self.connection.cursor()
+        self.connection.autocommit = True
+        sys.stdout.write(SUCCESS + "create connection")
+
+    def start_create_db(self):
+        self.create_user()
+        # self.cursor.execute(open("1_create_users.sql", "r").read())
+
+    def create_user(self):
+        str_user_creating = "CREATE USER {0} WITH PASSWORD \'{1}\'; ".format(DEFAULT_DB_USER, DB_USER_PWD)
+        self.cursor.execute(str_user_creating)
+
+
+if __name__ == '__main__':
+    db = DataBase()
+    db.start_create_db()
