6762f17e83661ef3ac97003ea4d001efcb89f312
anton <avodichenkov@gmail.com>
2023-11-22 19:23:06 +0300

    comment

--- scripts/db/db_creator.py
+++ scripts/db/db_creator.py
@@ -270,7 +270,7 @@ def make_pg_restore(backup_dir, db_name, file_name, db_user, db_host, db_schemas
     backup_call = ['pg_restore', '-Fc', '-h', db_host, '-U', db_user, '-d',
                    db_name, "{0}/{1}".format(backup_dir, file_name)]
 
-    db = DataBase(create_connection_db(db_name, db_user, DB_USER_PWD)) # if drop db move under create_database
+    db = DataBase(create_connection_db(db_name, db_user, DB_USER_PWD)) # if drop db delete this
     # пока удаляю каждую схему, можно просто удалять БД, закомментировано ниже
     for db_schema in db_schemas:
         db.query("DROP SCHEMA IF EXISTS {0} CASCADE;".format(db_schema))
