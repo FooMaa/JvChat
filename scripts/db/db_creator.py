f05ac67e804ab52850cf00c1ed2520cc00b738b9
anton <avodichenkov@gmail.com>
2023-11-22 07:25:44 +0300

    good

--- scripts/db/db_creator.py
+++ scripts/db/db_creator.py
@@ -263,7 +263,7 @@ def make_dump_db(backup_dir, db_name, file_name, db_user, db_host):
 def make_pg_restore(backup_dir, db_name, file_name, db_user, db_host, db_schemas):
     #sys.stdout.write(PENDING + "Restore database {0} use dump".format(db_name))
 
-    if os.path.isfile("{0}/{1}".format(backup_dir, file_name)):
+    if not os.path.isfile("{0}/{1}".format(backup_dir, file_name)):
         sys.stdout.write(PENDING + "Restore database {0} use dump".format(db_name))
         sys.stdout.flush()
         sys.stdout.write(FAIL + "Restore database {0} use dump".format(db_name) + '\n')
