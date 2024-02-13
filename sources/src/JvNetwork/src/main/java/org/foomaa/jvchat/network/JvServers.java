c04d9e11c38c77256d40ec421c6c2302050568a7
unknown <avodichenkov@gmail.com>
2024-02-13 08:09:33 +0300

    Начал строить соединение

--- sources/src/JvNetwork/src/main/java/org/foomaa/jvchat/network/JvServers.java
+++ sources/src/JvNetwork/src/main/java/org/foomaa/jvchat/network/JvServers.java
@@ -3,18 +3,27 @@ package org.foomaa.jvchat.network;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
 
-public class JvServers extends Thread {
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class JvServers {
     private static JvServers instance;
-    private static ServerSocket serverSocket;
-    private static BufferedReader fromSocket;
-    private static BufferedWriter inSocket;
+    private static final int port = 4004;
 
-    private JvServers() throws IOException {
-        serverSocket = new ServerSocket(4004);
-        System.out.println("Open socket");
-//        serverSocket.setSoTimeout(10000);
+    private JvServers() {
+        try {
+            ServerSocket servSocket = new ServerSocket(port);
+            while (true) {
+                // Получив соединение начинаем работать с сокетом
+                Socket fromClientSocket = servSocket.accept();
+                // Стартуем новый поток для обработки запроса клиента
+                new SocketThread(fromClientSocket).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 
     public static JvServers getInstance() throws IOException {
@@ -24,3 +33,41 @@ public class JvServers extends Thread {
         return instance;
     }
 }
+
+class SocketThread extends Thread
+{
+    private Socket fromClientSocket;
+
+    public SocketThread(Socket fromClientSocket) {
+        this.fromClientSocket = fromClientSocket;
+    }
+
+    @Override
+    public void run() {
+        // Автоматически будут закрыты все ресурсы
+        try (Socket localSocket = fromClientSocket;
+             PrintWriter pw = new PrintWriter(localSocket.getOutputStream(), true);
+             BufferedReader br = new BufferedReader(new InputStreamReader(localSocket.getInputStream()))) {
+
+            // Читаем сообщения от клиента до тех пор пока он не скажет "bye"
+            String str;
+            while ((str = br.readLine()) != null) {
+                // Печатаем сообщение
+                System.out.println("The message: " + str);
+                // Сравниваем с "bye" и если это так - выходим из цикла и закрываем соединение
+                if (str.equals("bye")) {
+                    // Тоже говорим клиенту "bye" и выходим из цикла
+                    pw.println("bye");
+                    break;
+                } else {
+                    // Посылаем клиенту ответ
+                    str = "Server returns " + str;
+                    pw.println(str);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
