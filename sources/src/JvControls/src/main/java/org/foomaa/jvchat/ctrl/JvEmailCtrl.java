e143f106b4dca06f02831ac1cca7aad9341c0830
Anton <avodichenkov@gmail.com>
2024-03-18 20:22:05 +0300

    Rafact

---
+++ sources/src/JvControls/src/main/java/org/foomaa/jvchat/ctrl/JvEmailCtrl.java
@@ -0,0 +1,22 @@
+package org.foomaa.jvchat.ctrl;
+
+import org.foomaa.jvchat.network.JvEmailProcessor;
+import org.foomaa.jvchat.settings.JvMainSettings;
+
+public class JvEmailCtrl {
+    private static JvEmailCtrl instance;
+    private static JvEmailProcessor email;
+
+    private JvEmailCtrl() {
+        if (JvMainSettings.getProfile() == JvMainSettings.TypeProfiles.SERVERS) {
+            email = JvEmailProcessor.getInstance();
+        }
+    }
+
+    public static JvEmailCtrl getInstance() {
+        if (instance == null) {
+            instance = new JvEmailCtrl();
+        }
+        return instance;
+    }
+}
