99edc64818a19f366b3cf84056bb7ad53195757a
Anton <134139503+FooMaa@users.noreply.github.com>
2024-03-27 07:36:38 +0300

    Убрал id из временных таблиц (#37)

--- scripts/db/chat_schema/30_create_verify_registartion_email.sql
+++ scripts/db/chat_schema/30_create_verify_registartion_email.sql
@@ -3,15 +3,13 @@ SET CLIENT_ENCODING TO 'UTF-8';
 DROP TABLE IF EXISTS chat_schema.verify_registration_email CASCADE;
 
 CREATE TABLE chat_schema.verify_registration_email (
-    id          serial, -- убрать если выяснится, что его мало
     email       character varying,
     code        character varying,
     datetime        timestamp NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (id)
+    PRIMARY KEY (email)
 );
 
 COMMENT ON TABLE chat_schema.verify_registration_email IS 'Временная таблица для подтверждения почты при регистрации';
-COMMENT ON COLUMN chat_schema.verify_registration_email.id IS 'ID записи';
 COMMENT ON COLUMN chat_schema.verify_registration_email.email IS 'Почта пользователя';
 COMMENT ON COLUMN chat_schema.verify_registration_email.code IS 'Проверочный код пользователя';
 COMMENT ON COLUMN chat_schema.verify_registration_email.datetime IS 'Штамп времени';
