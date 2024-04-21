a4cabfab5fac562e3bb7abc771a1bf21524e33b5
Anton <avodichenkov@gmail.com>
2024-04-21 16:22:12 +0300

    .

--- sources/src/JvStartPoint/src/main/java/org/foomaa/jvchat/startpoint/JvStartPoint.java
+++ sources/src/JvStartPoint/src/main/java/org/foomaa/jvchat/startpoint/JvStartPoint.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JvStartPoint implements ApplicationRunner {
-
     public static void main(String[] args) {
         SpringApplication.run( JvStartPoint.class, args );
     }
