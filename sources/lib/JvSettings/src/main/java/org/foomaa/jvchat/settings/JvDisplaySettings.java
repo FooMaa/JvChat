e0edacd06fcb853ee0078143f9911b4893f4d3fe
Anton <avodichenkov@gmail.com>
2024-05-19 21:09:56 +0300

    X 0:0

--- sources/lib/JvSettings/src/main/java/org/foomaa/jvchat/settings/JvDisplaySettings.java
+++ sources/lib/JvSettings/src/main/java/org/foomaa/jvchat/settings/JvDisplaySettings.java
@@ -16,7 +16,7 @@ public class JvDisplaySettings {
             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
             heightScreen = screenSize.height;
             widthScreen = screenSize.width;
-        } catch (Throwable t) {
+        } catch (HeadlessException exception) {
             System.out.println("No X :0:0 find");
         }
     }
