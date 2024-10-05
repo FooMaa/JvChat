243655f0d03cccf0395bd4feb02c03e8a5766b00
FooMaa <avodichenkov@gmail.com>
2024-10-05 23:25:06 +0300

    online ref 1

--- sources/src/JvControls/src/main/java/org/foomaa/jvchat/ctrl/JvOnlineServersCtrl.java
+++ sources/src/JvControls/src/main/java/org/foomaa/jvchat/ctrl/JvOnlineServersCtrl.java
@@ -10,32 +10,25 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
 
-// TODO(VAD): свести в одну структуру
+
 public class JvOnlineServersCtrl {
     private static JvOnlineServersCtrl instance;
-
-    private final Map<String, LocalDateTime> usersOnline;
-    private final Map<JvServersSocketThreadCtrl, CheckerOnline> listCheckerOnline;
-
+    private final List<CheckerOnline> listCheckerOnline;
     private final int intervalSecondsAfterLastSending;
-    private final int updateIntervalSecondsAfterLast;
+    private final int intervalSecondsAfterLastUpdate;
 
     private static class CheckerOnline {
+        public String login;
+        public JvServersSocketThreadCtrl thread;
         public boolean isSending;
-        public LocalDateTime dateTime;
-
-        CheckerOnline (boolean newIsSending, LocalDateTime newDateTime) {
-            isSending = newIsSending;
-            dateTime = newDateTime;
-        }
+        public LocalDateTime dateTimeSending;
+        public LocalDateTime dateTimeUpdating;
     }
 
-
     private JvOnlineServersCtrl() {
-        usersOnline = new HashMap<>();
-        listCheckerOnline = new HashMap<>();
+        listCheckerOnline = new ArrayList<>();
         intervalSecondsAfterLastSending = 10;
-        updateIntervalSecondsAfterLast = 30;
+        intervalSecondsAfterLastUpdate = 30;
     }
 
     static JvOnlineServersCtrl getInstance() {
@@ -52,7 +45,10 @@ public class JvOnlineServersCtrl {
 
         for (Map<JvDbGlobalDefines.LineKeys, String> map : dataFromDb) {
             for (String login : map.values()) {
-                usersOnline.put(login, LocalDateTime.now());
+                CheckerOnline onlineUser = new CheckerOnline();
+                onlineUser.login = login;
+                onlineUser.dateTimeUpdating = LocalDateTime.now();
+                listCheckerOnline.add(onlineUser);
             }
         }
 
@@ -60,16 +56,30 @@ public class JvOnlineServersCtrl {
     }
 
     public void addUsersOnline(String userLogin) {
-        if (!usersOnline.containsKey(userLogin)) {
-            usersOnline.put(userLogin, LocalDateTime.now());
-            saveStatusOnline(userLogin, JvMainChatsGlobalDefines.TypeStatusOnline.Online);
+        for (CheckerOnline onlineUser : listCheckerOnline) {
+            if (Objects.equals(onlineUser.login, userLogin)) {
+                onlineUser.dateTimeUpdating = LocalDateTime.now();
+                return;
+            }
         }
+
+        CheckerOnline onlineUser = new CheckerOnline();
+        onlineUser.login = userLogin;
+        onlineUser.dateTimeUpdating = LocalDateTime.now();
+        listCheckerOnline.add(onlineUser);
+
+        saveStatusOnline(userLogin, JvMainChatsGlobalDefines.TypeStatusOnline.Online);
     }
 
     public void removeUsersOnline(String userLogin) {
-        if (usersOnline.containsKey(userLogin)) {
-            usersOnline.remove(userLogin);
-            saveStatusOnline(userLogin, JvMainChatsGlobalDefines.TypeStatusOnline.Offline);
+        boolean flagContains = false;
+
+        for (CheckerOnline onlineUser : listCheckerOnline) {
+            if (Objects.equals(onlineUser.login, userLogin)) {
+                listCheckerOnline.remove(onlineUser);
+                saveStatusOnline(userLogin, JvMainChatsGlobalDefines.TypeStatusOnline.Offline);
+                break;
+            }
         }
     }
 
@@ -87,48 +97,48 @@ public class JvOnlineServersCtrl {
     private void runningSendListenOnline() {
         Runnable listenOnline = () -> {
             while (true) {
-                sendListeningPackage();
+//                sendListeningPackage();
             }
         };
 
         Thread thread = new Thread(listenOnline);
         thread.start();
     }
-
-    private void sendListeningPackage() {
-        LinkedList<JvServersSocketThreadCtrl> connectionList =
-                JvGetterControls.getInstance().getBeanNetworkCtrl().getConnectionList();
-
-        for (JvServersSocketThreadCtrl socketThreadCtrl : connectionList) {
-            try {
-                preSendingTasks(socketThreadCtrl);
-            } catch (InterruptedException exception) {
-                JvLog.write(JvLog.TypeLog.Error, "Здесь не удалось выполнить wait()");
-            }
-
-            JvGetterControls.getInstance().getBeanSendMessagesCtrl().sendMessage(
-                    JvDefinesMessages.TypeMessage.CheckOnlineUserRequest,
-                    JvGetterSettings.getInstance().getBeanServersInfoSettings().getIp(),
-                    socketThreadCtrl);
-
-            if (!listCheckerOnline.containsKey(socketThreadCtrl)) {
-                CheckerOnline checkerOnline = new CheckerOnline(true, LocalDateTime.now());
-                listCheckerOnline.put(socketThreadCtrl, checkerOnline);
-            }
-        }
-    }
-
-    private void preSendingTasks(JvServersSocketThreadCtrl socketThreadCtrl) throws InterruptedException {
-        if (listCheckerOnline.containsKey(socketThreadCtrl)) {
-            boolean flagSending = listCheckerOnline.get(socketThreadCtrl).isSending;
-            LocalDateTime lastSendingDateTime = listCheckerOnline.get(socketThreadCtrl).dateTime;
-
-            Duration duration = Duration.between(lastSendingDateTime, LocalDateTime.now());
-            long secondsAfterLastSending = duration.getSeconds();
-
-            if (flagSending && secondsAfterLastSending < intervalSecondsAfterLastSending) {
-                Thread.sleep(intervalSecondsAfterLastSending - secondsAfterLastSending);
-            }
-        }
-    }
+// TODO (VAD): доделать
+//    private void sendListeningPackage() {
+//        LinkedList<JvServersSocketThreadCtrl> connectionList =
+//                JvGetterControls.getInstance().getBeanNetworkCtrl().getConnectionList();
+//
+//        for (JvServersSocketThreadCtrl socketThreadCtrl : connectionList) {
+//            try {
+//                preSendingTasks(socketThreadCtrl);
+//            } catch (InterruptedException exception) {
+//                JvLog.write(JvLog.TypeLog.Error, "Здесь не удалось выполнить wait()");
+//            }
+//
+//            JvGetterControls.getInstance().getBeanSendMessagesCtrl().sendMessage(
+//                    JvDefinesMessages.TypeMessage.CheckOnlineUserRequest,
+//                    JvGetterSettings.getInstance().getBeanServersInfoSettings().getIp(),
+//                    socketThreadCtrl);
+//
+//            if (!listCheckerOnline.containsKey(socketThreadCtrl)) {
+//                CheckerOnline checkerOnline = new CheckerOnline(true, LocalDateTime.now());
+//                listCheckerOnline.put(socketThreadCtrl, checkerOnline);
+//            }
+//        }
+//    }
+//
+//    private void preSendingTasks(JvServersSocketThreadCtrl socketThreadCtrl) throws InterruptedException {
+//        if (listCheckerOnline.containsKey(socketThreadCtrl)) {
+//            boolean flagSending = listCheckerOnline.get(socketThreadCtrl).isSending;
+//            LocalDateTime lastSendingDateTime = listCheckerOnline.get(socketThreadCtrl).dateTime;
+//
+//            Duration duration = Duration.between(lastSendingDateTime, LocalDateTime.now());
+//            long secondsAfterLastSending = duration.getSeconds();
+//
+//            if (flagSending && secondsAfterLastSending < intervalSecondsAfterLastSending) {
+//                Thread.sleep(intervalSecondsAfterLastSending - secondsAfterLastSending);
+//            }
+//        }
+//    }
 }
