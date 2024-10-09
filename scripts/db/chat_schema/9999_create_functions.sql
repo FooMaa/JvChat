5a904800376a3689b679f12dee96c68094c31d72
anton <avodichenkov@gmail.com>
2024-10-09 12:46:45 +0300

    function

--- scripts/db/chat_schema/9999_create_functions.sql
+++ scripts/db/chat_schema/9999_create_functions.sql
@@ -345,6 +345,25 @@ BEGIN
 END;
 $BODY$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION chat_schema.online_users_info_get_time_by_user_login(f_login character varying)
+    RETURNS timestamp AS
+$BODY$
+DECLARE
+    rv timestamp;
+BEGIN
+    SELECT chat_schema.online_users_info.last_online_time
+    INTO rv
+    FROM chat_schema.online_users_info 
+    LEFT JOIN chat_schema.auth_users_info ON chat_schema.online_users_info.id_user = chat_schema.auth_users_info.id
+    WHERE chat_schema.auth_users_info.login = f_login;
+
+    IF found THEN
+        RETURN rv;
+    END IF;
+    RETURN NULL;
+END;
+$BODY$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION chat_schema.online_users_info_save (
     f_login         character varying,
     f_status        integer
