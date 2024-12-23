337403d22fa23661d4070d0cc2f89c32b9f1042e
anton <avodichenkov@gmail.com>
2024-12-23 13:39:22 +0300

    save

--- scripts/db/chat_schema/10_fill_auth_users_info.sql
+++ scripts/db/chat_schema/10_fill_auth_users_info.sql
@@ -1,5 +1,5 @@
 DELETE FROM chat_schema.auth_users_info;
 
 INSERT INTO chat_schema.auth_users_info (login, email, uuid_user, password) VALUES ('foomaa', 'advokat378@mail.ru',  '1d70a6a5-7461-459b-a7c0-b018f6de57d0', '2120ea176dd154dd53820460561c825d3b9a70d8d46b7da5605fb455adfcbb1d');
-INSERT INTO chat_schema.auth_users_info (login, email, uuid_user, password) VALUES ('sgubr', 'advokat1@mail.ru',  '1d70a6a5-7461-459b-a7c0-b018f6de57d0', '2120ea176dd154dd53820460561c825d3b9a70d8d46b7da5605fb455adfcbb1d');
-INSERT INTO chat_schema.auth_users_info (login, email, uuid_user, password) VALUES ('mike', 'advokat@mail.ru',  '1d70a6a5-7461-459b-a7c0-b018f6de57d0', '2120ea176dd154dd53820460561c825d3b9a70d8d46b7da5605fb455adfcbb1d');
+INSERT INTO chat_schema.auth_users_info (login, email, uuid_user, password) VALUES ('sgubr', 'advokat1@mail.ru',  '1d70a6a5-7461-459b-a7c0-b018f6de57d1', '2120ea176dd154dd53820460561c825d3b9a70d8d46b7da5605fb455adfcbb1d');
+INSERT INTO chat_schema.auth_users_info (login, email, uuid_user, password) VALUES ('mike', 'advokat@mail.ru',  '1d70a6a5-7461-459b-a7c0-b018f6de57d2', '2120ea176dd154dd53820460561c825d3b9a70d8d46b7da5605fb455adfcbb1d');
