725f50065bcd11e1edd0c97b70e13489968f3fe9
user <avodichenkov@gmail.com>
2026-03-26 18:07:59 +0300

    Сделал конфиг для форматтера

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ subprojects {
 
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         java {
-            eclipse("4.21.0")
+            eclipse("4.21.0").configFile(rootProject.file("config/eclipse-format.xml"))
             indentWithSpaces(4)
             removeUnusedImports()
             importOrder(
